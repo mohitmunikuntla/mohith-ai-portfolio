0632c874a3b4ad6912d9fec38a028aaf16a683bf
Mohit Munikuntla <mohitmunikuntla@Mohits-MacBook-Pro.local>
2026-03-11 15:36:03 +0530

    fix: correct App import and disable TS checking for portfolio file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import MohithAIPortfolio from "./mohith_ai_architect_portfolio";
+import MohithAIPortfolio from "./mohith_ai_portfolio";
 
 export default function App() {
   return <MohithAIPortfolio />;
